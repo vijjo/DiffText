57f0c2cafa1af174ba5f4e7cf58ad331ff0de377
Vijjo <31264468+vijjo@users.noreply.github.com>
2023-04-22 12:07:04 +0800

    add columns showing whether the example is in dpd

--- process_csv_data.py
+++ process_csv_data.py
@@ -23,8 +23,16 @@ def print_diff(sent1, sent2):
     print('\n'.join(diff))
 
 
-def example_group(source, sutta, example):
-    group = {'source': source, 'sutta': sutta, 'example': example}
+def ratios(sent1, sent2):
+    simple = fuzz.ratio(sent1, sent2)
+    partial = fuzz.partial_ratio(sent1, sent2)
+    token_sort = fuzz.token_sort_ratio(sent1, sent2)
+    token_set = fuzz.token_set_ratio(sent1, sent2)
+    return [simple, partial, token_sort, token_set]
+
+
+def example_group(source, sutta, example, dpd) :
+    group = {'source': source, 'sutta': sutta, 'example': example, 'dpd': dpd}
     return group
 
 
@@ -44,6 +52,7 @@ for i in range(1, 7):
     headings.append(f'u{i}_source')
     headings.append(f'u{i}_sutta')
     headings.append(f'u{i}_example')
+    headings.append(f'u{i}_dpd')
 # pprint(headings)
 
 with open(csv_file) as f, open("unified_data.csv", "w") as f_out, open('deleted_example.txt', 'w') as deleted_out, open('unmodified_entry.txt', 'w') as unmodified_out:
@@ -53,19 +62,21 @@ with open(csv_file) as f, open("unified_data.csv", "w") as f_out, open('deleted_
     # read csv into dictionary
     for index, row in enumerate(dict_reader):
         total_entry += 1
-        # if index == 1020:
+
+        # if index == 100:
         #     break
+
         dpd_list = []
         for i in range(1, 3):
             if row[f'DPD-Example{i}']:
                 group = example_group(
-                    row[f'DPD-Source{i}'], row[f'DPD-Sutta{i}'], row[f'DPD-Example{i}'])
+                    row[f'DPD-Source{i}'], row[f'DPD-Sutta{i}'], row[f'DPD-Example{i}'], True)
                 dpd_list.append(group)
         example_list = []
         for i in range(1, 5):
             if row[f'Example{i}']:
                 group = example_group(
-                    row[f'Source{i}'], row[f'Sutta{i}'], row[f'Example{i}'])
+                    row[f'Source{i}'], row[f'Sutta{i}'], row[f'Example{i}'], False)
                 example_list.append(group)
         # unified_list = dpd_list + example_list
         # original_list = unified_list.copy()
@@ -81,8 +92,9 @@ with open(csv_file) as f, open("unified_data.csv", "w") as f_out, open('deleted_
                 example_cleaned = clean_sentence(unit['example'])
                 dpd_cleaned = clean_sentence(dpd['example'])
                 simple_ratio = fuzz.ratio(example_cleaned, dpd_cleaned)
-                partial_ratio = fuzz.partial_ratio(
-                    example_cleaned, dpd_cleaned)
+                partial_ratio = fuzz.partial_ratio(example_cleaned, dpd_cleaned)
+                # print(ratios(example_cleaned, dpd_cleaned))
+                # print_diff(example_cleaned, dpd_cleaned)
                 if simple_ratio >= simple_bound or partial_ratio >= partial_bound:
                     removed = True
                     deleted_list.append(example_cleaned)
@@ -99,19 +111,13 @@ with open(csv_file) as f, open("unified_data.csv", "w") as f_out, open('deleted_
         else:
             unmodified_out.write(f"{row['ID']}\t{row['Pāli1']}\n")
         unified_list = dpd_list + example_list
-        # if simple_ratio >= 96 or partial_ratio >= 95:
-        #     pprint(original_list)
-        #     print("--------------")
-        #     pprint(unified_list)
-        #     print(simple_ratio, partial_ratio)
-        # if count == 2:
-        #     break
 
         for i, unified in enumerate(unified_list):
             i += 1
             row[f'u{i}_source'] = unified['source']
             row[f'u{i}_sutta'] = unified['sutta']
             row[f'u{i}_example'] = unified['example']
+            row[f'u{i}_dpd'] = unified['dpd']
         dict_writer.writerow(row)
 
 print(f'Total number of examples: {total_example}')
