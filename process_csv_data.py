2660e61eedfcfb4dfd25351af8918f94169bb5c4
Vijjo <31264468+vijjo@users.noreply.github.com>
2023-04-28 11:54:44 +0800

    filter out NAMES

--- process_csv_data.py
+++ process_csv_data.py
@@ -8,11 +8,25 @@ import re
 
 def clean_sentence(example):
     example = re.sub(r'<[^>]+>', '', example)
-    example = re.sub(r'\b(bhante|bhikkhave)\b', '', example)
+    # example = re.sub(r'\b(bhante|bhikkhave)\b', '', example)
     example = example.replace('&nbsp;', ' ')
     example = example.translate(str.maketrans('', '', "'?.!-,"))
     example = re.sub(r'\s+', ' ', example)
-    return example
+    example = example.strip()
+    words = example.split()
+    result_words = []
+    for word in words:
+        if word not in NAMES:
+            result_words.append(word) 
+        # else:
+        #     print(word)
+    filtered_example = ' '.join(result_words)
+    # filtered_example = filtered_example.strip()
+    # if example != filtered_example:
+    #     print(example)
+    #     print(filtered_example)
+    #     print('------------------')
+    return filtered_example
 
 
 def show_diff(sent1, sent2):
@@ -64,7 +78,8 @@ with open('names.txt') as f:
     for row in dict_reader:
         name = re.sub(r'\s[\d\.\s]+$', '', row['pali_1'])
         NAMES.append(name)
-pprint(NAMES)
+NAMES = NAMES + ['bhante', 'bhikkhave']
+# pprint(NAMES)
 
 with open(csv_file) as f, \
         open('output/unified_data.csv', 'w') as f_out, \
@@ -89,7 +104,7 @@ with open(csv_file) as f, \
         sbs_class_examples = []
         sbs_examples = []
         other_examples = []
-        example_list = []
+        dps_list = []
         for i in range(1, 5):
             if row[f'sbs_example_{i}']:
                 group = example_group(
@@ -102,32 +117,35 @@ with open(csv_file) as f, \
                 else:
                     other_examples.append(group)
         if sbs_examples:
-            example_list = sbs_examples[0:1] + sbs_class_examples + sbs_examples[1:] + other_examples
+            dps_list = sbs_examples[0:1] + sbs_class_examples + sbs_examples[1:] + other_examples
         else:
-            example_list = sbs_class_examples + other_examples
+            dps_list = sbs_class_examples + other_examples
         deleted_list = []
         modified = False
-        for unit in example_list[:]:
+        for dps in dps_list[:]:
             total_example += 1
             removed = False
             for dpd in dpd_list:
-                example_cleaned = clean_sentence(unit['example'])
+                example_cleaned = clean_sentence(dps['example'])
                 dpd_cleaned = clean_sentence(dpd['example'])
                 simple_ratio = fuzz.ratio(example_cleaned, dpd_cleaned)
                 partial_ratio = fuzz.partial_ratio(
                     example_cleaned, dpd_cleaned)
                 if simple_ratio >= SIMPLE_BOUND or partial_ratio >= PARTIAL_BOUND or \
-                    (partial_ratio >= 94 and unit['source'] == dpd['source']):
-                    dpd['chant_pali'] = unit['chant_pali']
-                    dpd['chant_eng'] = unit['chant_eng']
-                    dpd['sbs_chapter'] = unit['sbs_chapter']
+                    (partial_ratio >= 94 and dps['source'] == dpd['source']):
+                    if dps['chant_pali']:
+                        dpd['chant_pali'] = dps['chant_pali']
+                    if dps['chant_eng']:
+                        dpd['chant_eng'] = dps['chant_eng']
+                    if dps['sbs_chapter']:
+                        dpd['sbs_chapter'] = dps['sbs_chapter']
                     removed = True
                     deleted_list.append(example_cleaned)
                     break
             if removed:
                 modified = True
                 count_deleted_example += 1
-                example_list.remove(unit)
+                dps_list.remove(dps)
         
         # write into txt files: deleted_examples & unmodified entries
         if modified:
@@ -139,7 +157,7 @@ with open(csv_file) as f, \
             unmodified_out.write(f"{row['id']}\t{row['pali_1']}\n")
         
         # write unified_data.csv
-        unified_list = dpd_list + example_list
+        unified_list = dpd_list + dps_list
         for i, unified in enumerate(unified_list):
             i += 1
             row[f'u{i}_source'] = unified['source']
